fea4c0b2784e182d1445d65b0d2ec54d5d8735eb
ArnavMK <kothariarnav9@gmail.com>
2025-04-20 00:04:53 +0100

    ui(inspector): Add component button added
    -The inspector panel now has an add component button at the bottom.
    
    -When clicked it prints to the terminal

--- src/ui/panels/inspector.rs
+++ src/ui/panels/inspector.rs
@@ -7,6 +7,16 @@ pub fn manage_inspector_panel(ctx: &mut egui::Context) -> f32 {
         .show(ctx, |ui| {
             
             ui.heading("Inspector");
+
+            ui.with_layout(
+                egui::Layout::bottom_up(Align::Center),
+                |ui| {
+                    if ui.button("Add Component").clicked() {
+                        println!("Component will be added later. (Maybe never)");
+                    }
+                }
+            )
+            
         })
     .response.rect.width()
 }
